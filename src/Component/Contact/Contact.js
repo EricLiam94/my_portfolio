bea23a6798d7b075c6c2a6250305cbe3f9fb2fb0
Eric Fox <ericliamhjx@gmail.com>
2020-04-23 15:12:47 +1000

    .env add

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,21 +1,20 @@
 import React from "react";
 import style from "./contact.module.css";
-import contact from "./mycontact";
 
 const Contact = () => {
   return (
     <div id="contact" className={style.container}>
       <div className={style.card}>
         <i className="fas fa-mobile-alt"></i>
-        {contact.phone}
+        {process.env.REACT_APP_PHONE}
       </div>
       <div className={style.card}>
         <i className="fas fa-envelope"></i>
-        {contact.email}
+        {process.env.REACT_APP_EMAIL}
       </div>
       <div className={style.card}>
         <i className="fab fa-github"></i>
-        {contact.github}
+        {process.env.REACT_APP_GITHUB}
       </div>
     </div>
   );
