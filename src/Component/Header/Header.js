a3143457140eeb95920c987a79abd2970763026a
Eric Fox <ericliamhjx@gmail.com>
2020-05-02 13:03:55 +1000

    change title

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import style from "./Header.module.css";
+import Rellax from "rellax";
 
 const Header = () => {
+  useEffect(() => {
+    var rellax = new Rellax(".rellax");
+  }, []);
+
   const clickHandler = () => {
     window.scrollTo(0, document.body.scrollHeight);
   };
@@ -12,9 +17,9 @@ const Header = () => {
         <span>Jingxin Huang </span> | <span> Eric</span>
         <div className={style.loc}> Melbourne </div>
         <div onClick={clickHandler} className={style.icon}>
-          <i className="fas fa-mobile-alt"></i>
-          <i className="fas fa-envelope"></i>
-          <i className="fab fa-github"></i>
+          <i className="fas fa-mobile-alt rellax" data-rellax-speed="-2"></i>
+          <i className="fas fa-envelope rellax" data-rellax-speed="-2"></i>
+          <i className="fab fa-github rellax" data-rellax-speed="-2"></i>
         </div>
       </div>
       <div className={style.bg}> </div>
